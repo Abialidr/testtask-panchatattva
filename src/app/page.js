2fe1402f6cfff0271322e2ef244060c2b158aae5
Abialidr <luckybelt79@gmail.com>
2023-10-19 19:54:38 +0530

    added chhanges

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,11 @@
+'use client';
 import Image from 'next/image';
 import styles from './page.module.css';
+import useWindowSize from '@rooks/use-window-size';
 
 export default function Home() {
+  const { innerWidth: windowWidth } = useWindowSize();
+
   return (
     <div className='mainDiv'>
       <div
@@ -16,7 +20,13 @@ export default function Home() {
       <div className={styles.cards1Div}>
         <div className={styles.card1}>
           <article className={styles.card1UpperPart}>
-            
+            <p className={styles.imgContainer}>
+              <img
+                src='/Group 1000001871.png'
+                alt=''
+              />
+            </p>
+            <p className={styles.CardName}>Ahaar</p>
           </article>
           <article className={styles.card1LowerPart}></article>
         </div>
